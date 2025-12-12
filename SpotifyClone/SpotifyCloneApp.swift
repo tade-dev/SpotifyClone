9ab756b4a5f35f02c31adaada1ebb4b5a43951da
Akintade Oluwaseun <akintadeseun816@gmail.com>
2025-12-12 17:03:10 +0000

    Initial Commit

---
+++ SpotifyClone/SpotifyCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpotifyCloneApp.swift
+//  SpotifyClone
+//
+//  Created by BSTAR on 12/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct SpotifyCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
